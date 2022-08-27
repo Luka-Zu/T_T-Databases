67f39c8b753fdf039a38f9271ab7fe16c6aa9a56
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-27 22:37:10 +0400

    m

--- main.py
+++ main.py
@@ -34,3 +34,48 @@ def select_all():
     return result
 
 
+def select_specific_columns(column_name):
+    connection = sqlite3.connect('cinema.db')
+    cursor = connection.cursor()
+    cursor.execute(f"""
+    SELECT {column_name} FROM "Seat"
+    """)
+    result = cursor.fetchall()
+    return result
+
+
+def select_with_condition(condition):
+    connection = sqlite3.connect('cinema.db')
+    cursor = connection.cursor()
+    cursor.execute(f"""
+    SELECT * FROM "Seat" WHERE {condition}
+    """)
+    result = cursor.fetchall()
+    return result
+
+
+def update_value():
+    connection = sqlite3.connect('cinema.db')
+    connection.execute("""
+    UPDATE "Seat" SET "taken"=1
+    """)
+    connection.commit()
+    connection.close()
+
+
+def update_value_with_cond(sett, cond):
+    connection = sqlite3.connect('cinema.db')
+    connection.execute(f"""
+    UPDATE "Seat" SET {sett} WHERE {cond} 
+    """)
+    connection.commit()
+    connection.close()
+
+
+def delete_record():
+    connection = sqlite3.connect('cinema.db')
+    connection.execute("""
+    DELETE FROM "Seat" WHERE seat_id="A4"
+    """)
+    connection.commit()
+    connection.close()
